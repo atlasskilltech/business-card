636ab58661450b3c16716191150391341ed7329c
ATLAS SkillTech University <tech@atlasuniversity.edu.in>
2026-02-06 11:31:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,18 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    plugins: [react()],
-    server: {
-      port: 3000,
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL,
-          changeOrigin: true
-        },
-        '/uploads': {
-          target: env.VITE_API_URL,
-          changeOrigin: true
-        }
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://business-card-api.atlasskilltech.app/api',
+        changeOrigin: true
+      },
+      '/uploads': {
+        target: 'https://business-card-api.atlasskilltech.app/api',
+        changeOrigin: true
       }
     }
   }
